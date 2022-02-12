db37d79b740ed260530131740907f8a4685d3104
Hassan Gujjar <hbsgujjar111h@gmail.com>
2022-02-12 13:13:09 +0500

    Hassan ~ detail page

--- lib/app screens/khubaib/detail_page.dart
+++ lib/app screens/khubaib/detail_page.dart
@@ -1,4 +1,3 @@
-import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:mik_and_min/utils/config.dart';
 import 'package:mik_and_min/utils/dynamic_sizes.dart';
@@ -19,13 +18,12 @@ class _DetailPageState extends State<DetailPage> {
         backgroundColor: CustomColors.customWhite,
         body: Column(
           children: [
-             appBar(context,bottomtext: true,title:"Jacket & Sweaters"),
+            appBar(context, bottomtext: true, title: "Jacket & Sweaters"),
             Divider(
               thickness: CustomSizes().dynamicWidth(context, 0.007),
             ),
             Expanded(
               child: GridView.builder(
-             
                 primary: true,
                 shrinkWrap: true,
                 itemCount: 6,
@@ -107,107 +105,96 @@ Widget chip(context, title) {
   );
 }
 
-Widget card (context) {
+Widget card(context) {
   return Container(
-                   
-                   
-                    child: Column(
-                      children: [
-                        Container(
-                            width: CustomSizes().dynamicWidth(context, 0.42),
-                            height: CustomSizes().dynamicHeight(context, 0.2),
-                            color: CustomColors.customWhite,
-                            child: Center(
-                              child: text(context, "image", 0.02,
-                                  CustomColors.customBlack),
-                            )),
-                        Container(
-                          width: CustomSizes().dynamicWidth(context, 0.42),
-                          height: CustomSizes().dynamicHeight(context, 0.07),
-                          color: CustomColors.customGrey.withOpacity(0.2),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              text(context, "CocoBee", 0.025,
-                                  CustomColors.customBlack),
-                              text(context, "Teal Turquoise Hoodie", 0.025,
-                                  CustomColors.customBlack,
-                                  bold: true),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  CustomSizes().widthBox(context, 0.05),
-                                  Text(
-                                    "Rs.3190",
-                                    style: TextStyle(
-                                        color: CustomColors.customGrey,
-                                        fontSize: CustomSizes()
-                                            .dynamicHeight(context, 0.012),
-                                        decoration: TextDecoration.lineThrough),
-                                  ),
-                                  text(context, "Rs.1569", 0.025,
-                                      CustomColors.customPink),
-                                  Padding(
-                                    padding: EdgeInsets.only(
-                                      right: CustomSizes()
-                                          .dynamicWidth(context, 0.02),
-                                    ),
-                                    child: Icon(
-                                      Icons.star_border_outlined,
-                                      size: CustomSizes()
-                                          .dynamicHeight(context, 0.025),
-                                    ),
-                                  ),
-                                
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                       
-                        Divider(
-                         thickness: CustomSizes().dynamicWidth(context, 0.002),
-                        ),
-                      ],
-                    ),
-                  );
-}
-
-Widget appBar(context,{bottomtext = false,title}) {
-  return Padding(
-              padding: EdgeInsets.only(
-                  top: CustomSizes().dynamicHeight(context, 0.01)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    child: Column(
+      children: [
+        Container(
+            width: CustomSizes().dynamicWidth(context, 0.42),
+            height: CustomSizes().dynamicHeight(context, 0.2),
+            color: CustomColors.customWhite,
+            child: Center(
+              child: text(context, "image", 0.02, CustomColors.customBlack),
+            )),
+        Container(
+          width: CustomSizes().dynamicWidth(context, 0.42),
+          height: CustomSizes().dynamicHeight(context, 0.07),
+          color: CustomColors.customGrey.withOpacity(0.2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              text(context, "CocoBee", 0.025, CustomColors.customBlack),
+              text(context, "Teal Turquoise Hoodie", 0.025,
+                  CustomColors.customBlack,
+                  bold: true),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Icon(
-                    Icons.arrow_back_ios_new_sharp,
-                    size: CustomSizes().dynamicWidth(context, 0.05),
+                  CustomSizes().widthBox(context, 0.05),
+                  Text(
+                    "Rs.3190",
+                    style: TextStyle(
+                        color: CustomColors.customGrey,
+                        fontSize: CustomSizes().dynamicHeight(context, 0.012),
+                        decoration: TextDecoration.lineThrough),
                   ),
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: CustomSizes().dynamicHeight(context, .029),
-                        backgroundColor: CustomColors.customBlue,
-                        child: CircleAvatar(
-                          backgroundColor: CustomColors.customWhite,
-                          radius: CustomSizes().dynamicHeight(context, .027),
-                        ),
-                      ),
-                      bottomtext== true ?text(context, title, 0.02,
-                          CustomColors.customBlack,
-                          bold: true): Container(),
-                    ],
-                  ),
-                  chip(context, "1-3 YEARS"),
-                  chip(context, "3-6 YEARS"),
-                  chip(context, "6+ YEARS"),
-                  Icon(
-                    Icons.menu,
-                    size: CustomSizes().dynamicWidth(context, 0.08),
+                  text(context, "Rs.1569", 0.025, CustomColors.customPink),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: CustomSizes().dynamicWidth(context, 0.02),
+                    ),
+                    child: Icon(
+                      Icons.star_border_outlined,
+                      size: CustomSizes().dynamicHeight(context, 0.025),
+                    ),
                   ),
                 ],
               ),
-            );
+            ],
+          ),
+        ),
+        Divider(
+          thickness: CustomSizes().dynamicWidth(context, 0.002),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget appBar(context, {bottomtext = false, title}) {
+  return Padding(
+    padding: EdgeInsets.only(top: CustomSizes().dynamicHeight(context, 0.01)),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(
+          Icons.arrow_back_ios_new_sharp,
+          size: CustomSizes().dynamicWidth(context, 0.05),
+        ),
+        Column(
+          children: [
+            CircleAvatar(
+              radius: CustomSizes().dynamicHeight(context, .029),
+              backgroundColor: CustomColors.customBlue,
+              child: CircleAvatar(
+                backgroundColor: CustomColors.customWhite,
+                radius: CustomSizes().dynamicHeight(context, .027),
+              ),
+            ),
+            bottomtext == true
+                ? text(context, title, 0.02, CustomColors.customBlack,
+                    bold: true)
+                : Container(),
+          ],
+        ),
+        chip(context, "1-3 YEARS"),
+        chip(context, "3-6 YEARS"),
+        chip(context, "6+ YEARS"),
+        Icon(
+          Icons.menu,
+          size: CustomSizes().dynamicWidth(context, 0.08),
+        ),
+      ],
+    ),
+  );
 }
